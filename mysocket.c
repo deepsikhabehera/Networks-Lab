2df14d2fbde4590ef21275a43394c5632aa7c5aa
anushka-2002 <anushkas12345@gmail.com>
2023-03-14 15:22:54 +0530

    mysend and myrecv

--- mysocket.c
+++ mysocket.c
@@ -1,19 +1,25 @@
 #include "mysocket.h"
 #include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
 
-#define MAX_RECV_LEN 5000
-#define MAX_SEND_LEN 1000
+#define MAX_LEN 5000
+#define NUM_MEGS 100
+
+static pthread_t R, S;
 
 typedef struct {
     int sockfd;
-    char msgs[MAX_SEND_LEN];
-    int front, rear;
+    char msgs[NUM_MEGS][MAX_LEN];
+    int front, rear, count;
 } Send_Message;
 
 typedef struct {
     int sockfd;
-    char msgs[MAX_RECV_LEN];
-    int front, rear;
+    char msgs[MAX_LEN];
+    int isempty;
 } Received_Message;
 
 Send_Message *send_msgs;
@@ -37,7 +43,6 @@ int my_socket(int domain, int type, int protocol)
     }
 
     // Initialize send_msgs and recv_msgs
-
     send_msgs = (Send_Message *) malloc(sizeof(Send_Message));
     send_msgs->sockfd = sockfd;
     send_msgs->front = 0;
@@ -45,13 +50,11 @@ int my_socket(int domain, int type, int protocol)
 
     recv_msgs = (Received_Message *) malloc(sizeof(Received_Message));
     recv_msgs->sockfd = sockfd;
-    recv_msgs->front = 0;
-    recv_msgs->rear = 0;
+    recv_msgs->isempty = 1;
 
     // Create threads R and S
-    pthread_t thread_R, thread_S;
-    pthread_create(&thread_R, NULL, thread_R, (void *) recv_msgs);
-    pthread_create(&thread_S, NULL, thread_S, (void *) send_msgs);
+    pthread_create(&R, NULL, thread_R, (void *) recv_msgs);
+    pthread_create(&S, NULL, thread_S, (void *) send_msgs);
 
     return sockfd;
 }
@@ -79,12 +82,43 @@ int my_connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen)
 int my_close(int sockfd)
 {
     // Terminate threads R and S
-    pthread_kill(thread_R, SIGTERM);
-    pthread_kill(thread_S, SIGTERM);
+    pthread_kill(R, SIGTERM);
+    pthread_kill(S, SIGTERM);
 
     // Clean up Send_Message and Received_Message tables
     free(send_msgs);
     free(recv_msgs);
 
     return close(sockfd);
+}
+
+ssize_t my_send(int sockfd, const void *buf, size_t len, int flags)
+{
+    // Block if send_msgs is full
+    while(send_msgs->count == NUM_MEGS)
+    {
+        sleep(5);
+    }
+
+    // Add message to send_msgs
+    strcpy(send_msgs->msgs[send_msgs->rear], buf);
+    send_msgs->rear = (send_msgs->rear + 1) % NUM_MEGS;
+    send_msgs->count++;
+
+    return len;
+}
+
+ssize_t my_recv(int sockfd, void *buf, size_t len, int flags)
+{
+    // Block if recv_msgs is empty
+    while(recv_msgs->isempty)
+    {
+        sleep(5);
+    }
+
+    // Remove message from recv_msgs
+    strcpy(buf, recv_msgs->msgs);
+    recv_msgs->isempty = 1;
+
+    return len;
 }
